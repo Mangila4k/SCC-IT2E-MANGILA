8f6afd08e7f8af9a8443c41f933aa8d0da80121f
Mangila4k <johnmicoleiii@gmail.com>
2024-08-14 17:52:08 +0800

    Ayaw panghilabot una ni nako nga java ohaha

---
+++ src/mangila/Mangila.java
@@ -0,0 +1,15 @@
+package mangila;
+
+import java.util.Scanner;
+        
+public class Mangila {
+
+    public static void main(String[] args) {
+      
+        Sales sls= new Sales();
+        sls.getSales();
+ 
+        
+    }
+    
+}
