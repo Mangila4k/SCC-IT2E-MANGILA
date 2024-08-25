31dd421fcfc054288d9dd71792ee2ba9ed2d39e7
Mangila4k <johnmicoleiii@gmail.com>
2024-08-25 23:15:03 +0800

    I add product class

--- src/mangila/Mangila.java
+++ src/mangila/Mangila.java
@@ -3,8 +3,16 @@ package mangila;
 public class Mangila {
 
     public static void main(String[] args) {
-        Grades gr = new Grades();
-        gr.addGrades(1011, "Micole Gwapo", 1.0, 2.2, 2.3, 1.3);
-        gr.viewGrades();
+//            Grades gr = new Grades();
+//            gr.addGrades(1011, "Micole Gwapo", 1.0, 2.2, 2.3, 1.3);
+//            gr.viewGrades();
+
+                Products pr = new Products();
+                pr.addProducts(1011, "Logod", 30.00, 60, 0);
+                pr.viewProducts();
+                
+                Products pr1 = new Products();
+                pr1.addProducts(1011, "Logod", 30.00, 0, 0);
+                pr1.viewProducts();
     }    
 }
