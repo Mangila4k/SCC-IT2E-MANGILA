6d4da5a4ceaa60fd535d6141d55ecc243f15eda6
Mangila4k <johnmicoleiii@gmail.com>
2024-08-25 22:47:20 +0800

    Ayaw panghilabot una ni nako nga java ohaha

--- src/mangila/Mangila.java
+++ src/mangila/Mangila.java
@@ -1,15 +1,10 @@
 package mangila;
 
-import java.util.Scanner;
-        
 public class Mangila {
 
     public static void main(String[] args) {
-      
-        Sales sls= new Sales();
-        sls.getSales();
- 
-        
-    }
-    
+        Grades gr = new Grades();
+        gr.addGrades(1011, "Micole Gwapo", 1.0, 2.2, 2.3, 1.3);
+        gr.viewGrades();
+    }    
 }
